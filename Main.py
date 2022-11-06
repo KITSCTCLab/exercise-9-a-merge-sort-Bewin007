ac8fe72a7dcbd36d4b3a81d6fb2efbc3ddb1a30a
Bewin Felix <99880789+Bewin007@users.noreply.github.com>
2022-11-06 21:44:02 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,27 +1,42 @@
 from typing import List
 
-def partition(arr,low,high):
-   i = ( low-1 )
-   pivot = arr[high] # pivot element
-   for j in range(low , high):
-      # If current element is smaller
-      if arr[j] <= pivot:
-         # increment
-         i = i+1
-         arr[i],arr[j] = arr[j],arr[i]
-   arr[i+1],arr[high] = arr[high],arr[i+1]
-   return ( i+1 )
-# sort
-def quick_sort(arr,low,high):
-   if low < high:
-      # index
-      pi = partition(arr,low,high)
-      # sort the partitions
-      quick_sort(arr, low, pi-1)
-      quick_sort(arr, pi+1, high)
-   return arr
+def merge_sort(myList) -> None:
+  if len(myList) > 1:
+        mid = len(myList) // 2
+        left = myList[:mid]
+        right = myList[mid:]
+
+        # Recursive call on each half
+        merge_sort(left)
+        merge_sort(right)
+
+        i = j = k = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+              # The value from the left half has been used
+              myList[k] = left[i]
+              # Move the iterator forward
+              i += 1
+            else:
+                myList[k] = right[j]
+                j += 1
+            # Move to the next slot
+            k += 1
+
+        # For all the remaining values
+        while i < len(left):
+            myList[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            myList[k]=right[j]
+            j += 1
+            k += 1
 
 
+# Do not change the following code
 input_data = input()
 data = []
 for item in input_data.split(', '):
@@ -29,4 +44,5 @@ for item in input_data.split(', '):
     data.append(int(item))
   elif item.lstrip("-").isnumeric():
     data.append(int(item))
-print(quick_sort(data, 0, len(data)-1))
+merge_sort(data)
+print(data)
